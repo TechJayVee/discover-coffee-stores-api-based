76bdd3ef5f4f4fbad51edaf7c607a8473284b2a8
Jayvee Ramos <97137481+TechJayVee@users.noreply.github.com>
2022-09-23 14:04:18 +0800

    finish layout with api call

---
+++ component/card.js
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/card.module.css";
+import cls from "classnames";
+const Card = (props) => {
+  return (
+    <div>
+      <Link href={props.href}>
+        <a className={styles.cardLink}>
+          <div className={cls("glass", styles.container)}>
+            <div className={styles.cardHeaderWrapper}>
+              <h2 className={styles.cardHeader}>{props.name}</h2>
+            </div>
+            <div className={styles.cardImageWrapper}>
+              <Image
+                src={props.imgUrl}
+                width={260}
+                height={160}
+                alt="Image-Card"
+              />
+              ;
+            </div>
+          </div>
+        </a>
+      </Link>
+    </div>
+  );
+};
+
+export default Card;
